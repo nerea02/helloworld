b66169623819c179db056c149e6d897e5f8d59ee
nerea <nereael02@gmail.com>
2024-10-30 19:30:31 -0300

    Primer commit: subiendo el proyecto HelloWorld

---
+++ src/main/java/com/example/helloworld/HelloWorldController.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author nerea
+ */
+@RestController
+public class HelloWorldController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hola Mundo";
+    }
+    
+}
